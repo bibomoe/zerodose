7060b62ee897034955c39b490215d99d1a83c5b5
bibomoe <abduldubelz@gmail.com>
2025-04-30 16:35:05 +0700

    update update_user view

--- application/views/input/edit_user.php
+++ application/views/input/edit_user.php
@@ -74,7 +74,7 @@
                                                         <!-- Dropdown untuk memilih province dan city (untuk kategori 7 dan 8) -->
                                                         <div class="form-group" id="province-city-group" style="display:<?= ($user->category == 7 || $user->category == 8) ? 'block' : 'none'; ?>;">
                                                             <?php
-                                                                if($user_category != 7){
+                                                                if($user->category != 7){
                                                             ?>
                                                                 <?= form_label('Province', 'province_id'); ?>
                                                                 <?= form_dropdown('province_id', $province_options, '', 'class="form-control" id="province_id"'); ?>
