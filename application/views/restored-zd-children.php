cb9b690aee4b9ba955e1c8aade0a331616e3393f
bibomoe <abduldubelz@gmail.com>
2025-05-19 13:51:10 +0700

    update longterm quarter target

--- application/views/restored-zd-children.php
+++ application/views/restored-zd-children.php
@@ -520,7 +520,7 @@
                                                             <!-- <th><?= $translations['tabelcoloumn1'] ?></th>
                                                             <th><?= $translations['tabelcoloumn6'] ?></th> -->
                                                             <th><?= $selected_district == 'all' ? $translations['tabelcoloumn1'] : $translations['tabelcoloumn1_2'] ?></th>
-                                                            <th><?= $selected_district == 'all' ? $translations['tabelcoloumn6'] : $translations['tabelcoloumn6_2'] ?> <?= $translations['text1_quarter'] ?> <?= $quarter; ?></th>
+                                                            <th><?= $selected_district == 'all' ? $translations['tabelcoloumn6'] : $translations['tabelcoloumn6_2'] ?></th>
                                                             <th><?= $translations['tabelcoloumn2'] ?> <?= $translations['text1_quarter'] ?> <?= $quarter; ?></th>
                                                             <th><?= $translations['tabelcoloumn3'] ?> <?= $translations['text1_quarter'] ?> <?= $quarter; ?></th>
                                                             <th><?= $translations['tabelcoloumn4'] ?> <?= $translations['text1_quarter'] ?> <?= $quarter; ?></th>
