f8ab5db5d7daccd68d9cd3ff65e4abec222c5155
bibomoe <abduldubelz@gmail.com>
2025-09-10 01:25:31 +0700

    update sidebar for language

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -673,7 +673,8 @@
 
             // Inisialisasi bahasa berdasarkan localStorage
             // let savedLang = localStorage.getItem("selectedLanguage") ;
-            let savedLang = <?= strval($this->session->userdata('language')) ?? 'en'; ?>.toString();
+            let savedLang = <?= json_encode($this->session->userdata('language') ?? 'en'); ?>;
+
             setLanguage(savedLang);
 
             // Jika bahasa tidak ada di localStorage, set bahasa default dan kirim ke server
