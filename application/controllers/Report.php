b291697ee53f2bf93ed3173d2773d3bac63352a5
bibomoe <abduldubelz@gmail.com>
2025-09-20 12:49:21 +0700

    update report page 20 sept 2025 narration report for targeted province

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -1198,7 +1198,9 @@ class Report extends CI_Controller {
 
         // Menambahkan jarak antara tabel pertama dan kedua
         $html .= '<br>';
-        $pdf->Ln(20);  // Menambah jarak 25 unit antara gambar dan judul
+        $html .= '</br>';
+        $html .= '<br>';
+        $html .= '</br>';
 
             // Tabel 2: Indikator Jangka Menengah
             $html .= '<h3 style="font-size:14pt;">Indikator Jangka Menengah</h3>';
