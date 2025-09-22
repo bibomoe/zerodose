3cf77761e12aad2e99d22c75bdc0df91537048ef
bibomoe <abduldubelz@gmail.com>
2025-09-23 05:47:31 +0700

    update revisi end

--- application/models/Immunization_model.php
+++ application/models/Immunization_model.php
@@ -276,11 +276,12 @@ class Immunization_model extends CI_Model {
         $province_ids = $this->get_targeted_province_ids();
 
         $this->db->select("
-            provinces.name_id AS district,
+            provinces.name_id AS district,  -- Kolom 'district' dipakai agar tetap cocok dengan view yang sudah ada
             SUM(immunization_data.dpt_hb_hib_1) AS total_dpt1,
             SUM(target_immunization.dpt_hb_hib_1_target) AS target_district
         ");
         $this->db->from('provinces');
+        // $this->db->join('cities', 'cities.province_id = provinces.id', 'left');
         $this->db->join('immunization_data', 'immunization_data.province_id = provinces.id AND immunization_data.year = ' . (int)$year, 'left');
         $this->db->join('target_immunization', 'target_immunization.province_id = provinces.id AND target_immunization.year = ' . (int)$year, 'left');
 
@@ -311,7 +312,6 @@ class Immunization_model extends CI_Model {
 
         return $result;
     }
-
 
     // Data total DPT-1 per distrik berdasarkan provinsi
     public function get_dpt1_by_district($province_id = 'all', $year = 2025, $max_month = 1) {
