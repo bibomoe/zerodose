06ee4e81ccc6aee15f1b6d9c74c1100ec917f12a
bibomoe <abduldubelz@gmail.com>
2025-10-01 21:36:05 +0700

    update report revision

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -562,12 +562,13 @@ class Report extends CI_Controller {
             $this->data['national_baseline_zd'] = $this->Report_model->get_zero_dose_by_province($selected_province, $selected_district);
         }
 
-        // var_dump($selected_month );
-        // exit;
+        var_dump($selected_month );
+        
 
         // ✅ Jika bulan ke-6 (Juni), bagi dua baseline
         if ($selected_month == '6') {
             $this->data['national_baseline_zd'] = $this->data['national_baseline_zd'] / 2;
+            exit;
         }
 
         // Total DPT 1 Kejar
